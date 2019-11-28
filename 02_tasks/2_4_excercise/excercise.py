76df439575d93fd1765130be0e514b3bd0359a49
martamxd <martamxd@wp.pl>
2019-11-28 15:56:28 +0100

    w/ Semaphore

--- 02_tasks/2_4_excercise/excercise.py
+++ 02_tasks/2_4_excercise/excercise.py
@@ -2,14 +2,14 @@ import sys
 import random
 import logging
 import threading
-
-
-logger = logging.getLogger(__name__)
+from threading import Semaphore
+ 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
+ 
+logger=logging.getLogger(__name__)
 resources = []
-
-
+s = Semaphore(value=0)
+ 
 def producer():
     """ Producer thread, append two item to the resources
     """
@@ -17,50 +17,54 @@ def producer():
     for i in range(2):
         r = random.random()
         resources.append(r)
-        logger.info(f'Produced an item {r}')
-
+        logger.info(f"Produced an item: {r}")
+        s.release()      
     pass
-
-
+ 
 def big_consumer():
     """ Big Consumer thread, consumes three items from the resources
     """
     global resources
-
-    # consumed_items=[]
-    # for i in range(3):
-    #     item = resources.pop(0)
-    #     consumed_items.append(item)
-
-    consumed_items = resources[0:3] #slice notation works as a loop
-    resources = resources[3:]
-    
-    logger.info(f'Consumed 3 items: {consumed_items}')
-
+  
+    consumed_items = []
+ 
+    for i in range(3):
+        s.acquire()
+        item = resources.pop(0)
+        consumed_items.append(item)
+        
+ 
+    logger.info(f"Consumed 3 items: {consumed_items} ")
     pass
-
-
+ 
 def small_consumer():
     """ Small Consumer thread, consumes one items from the resources
     """
+    s.acquire()
     product = resources.pop(0)
-    logger.info(f'Consumed {product} item')
+    logger.info(f"Consumed {product} item")
+    
     pass
-
-
+ 
 if __name__ == '__main__':
     """ Run following threads (in random order):
             - 1 big_consumers
             - 3 small_consumers
             - 3 producers
     """
-
     # TODO: run tasks in random order
-
-    producer()
-    producer()
-    big_consumer()
-    small_consumer()
-
+ 
+    tasks = [
+    threading.Thread(target=producer),
+    threading.Thread(target=big_consumer),
+    threading.Thread(target=small_consumer),
+    threading.Thread(target=producer),
+    ]
+ 
+    for t in tasks:
+        t.start()
+ 
+    for t in tasks:
+        t.join()
+ 
     assert len(resources) == 0
-
